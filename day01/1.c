f92669cf3c1b96d3af9b792d4125bf823685a292
guneyk98 <guneyk98@gmail.com>
2024-12-23 02:02:26 +0000

    day01

---
+++ day01/1.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINE_LEN 124
+
+size_t CountLines(FILE *fp);
+int CompareInts(const void *p, const void *q);
+
+int main(void)
+{
+	FILE *fp = fopen("input.txt", "r");
+	if (!fp) {
+		perror("Failed to open input.txt");
+		return EXIT_FAILURE;
+	}
+	
+	const size_t numLines = CountLines(fp);
+
+	int *L = malloc(sizeof(int) * numLines); 
+	int *R = malloc(sizeof(int) * numLines);
+	if (!R || !L) {
+		perror("Failed to allocate memory");
+		return EXIT_FAILURE;
+	}
+	
+	char lineStr[MAX_LINE_LEN];
+	for (size_t i = 0; i < numLines; ++i) {
+		fgets(lineStr, sizeof(lineStr), fp);
+		sscanf(lineStr, "%d %d", L+i, R+i);
+	}
+
+	qsort(L, numLines, sizeof(int), CompareInts);
+	qsort(R, numLines, sizeof(int), CompareInts); 
+	
+	int totalDistance = 0;
+	for (size_t i = 0; i < numLines; ++i)
+		totalDistance += abs(L[i] - R[i]);
+
+	printf("Total distance: %d\n", totalDistance);
+
+	// part 2
+	int similarity = 0;
+	for (size_t i = 0, j = 0; i < numLines; ++i) {
+		while (j < numLines && L[i] > R[j])
+			++j;
+		while (j < numLines && L[i] == R[j]) {
+			similarity += L[i];
+			++j;
+		}
+	}
+
+	printf("Similarity: %d\n", similarity);
+
+	free(L); free(R);
+	fclose(fp);
+}
+
+size_t CountLines(FILE *fp) {
+	char buffer[65536];
+	size_t count = 0;
+
+	while (!feof(fp)) {
+		size_t n = fread(buffer, 1, sizeof(buffer), fp);
+		if (ferror(fp)) {
+			perror("Error while reading file");
+			exit(EXIT_FAILURE);
+		}
+		for (size_t i = 0; i < n; ++i)
+			if (buffer[i] == '\n')
+				++count;
+	}
+	fseek(fp, 0L, SEEK_SET);
+	return count;
+}
+
+int CompareInts(const void *p, const void *q)
+{
+    return (*(int *)p > *(int *)q) - (*(int *)p < *(int *)q);
+}
